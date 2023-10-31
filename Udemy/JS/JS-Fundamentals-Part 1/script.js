2fb545ff0553581ecc1309e3778427ae924830e4
dianGrigorov <diangrigorov@gmail.com>
2023-10-31 14:16:13 +0200

    .

---
+++ Udemy/JS/JS-Fundamentals-Part 1/script.js
@@ -0,0 +1,14 @@
+// let country = "Bulgaria";
+// let continent = "Europe";
+// let population = 6_000_000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let isIsland = false;
+// let language;
+
+// console.log(isIsland);
+// console.log(language);
+ 
