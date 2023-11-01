9c75d35c734d9a8cfdf6d75f3f38f174c5171648
dianGrigorov <diangrigorov@gmail.com>
2023-11-01 14:26:22 +0200

    .

--- Udemy/JS/JS-Fundamentals-Part 1/script.js
+++ Udemy/JS/JS-Fundamentals-Part 1/script.js
@@ -13,4 +13,17 @@ console.log(isIsland);
 console.log(language);
 
 const language = "Bulgarian";
+
+
+let population = 7_000_000 / 2;
+population++;
+console.log(population);
+
+let FinlandPopulation = 6_000_000;
+console.log(FinlandPopulation > population);
+let averagePopulation = 33_000_000;
+console.log(averagePopulation > population);
+
+const description = "Portugal is in Europe, and its 11 million people speak portuguese";
+console.log(description);
  */
