086d2ff2509896346903a269c34c8e1c4dcb8755
dianGrigorov <diangrigorov@gmail.com>
2023-11-09 07:10:32 +0200

    .

--- Udemy/JS/JS-Fundamentals-Part 1/script.js
+++ Udemy/JS/JS-Fundamentals-Part 1/script.js
@@ -76,5 +76,15 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's! (${BMIMark})`);
 }
-*/
 
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+    console.log(`Dolphins win the trophy`);
+} else if (scoreKoalas > scoreDolphins) {
+    console.log(`Koalas win the trophy`);
+} else {
+    console.log(`Both win the trophy`);
+}
+*/
