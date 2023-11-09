2a86c897c57e53a74c53eb288b566cd009d28317
dianGrigorov <diangrigorov@gmail.com>
2023-11-09 07:36:31 +0200

    Update script.js

--- Udemy/JS/JS-Fundamentals-Part 1/script.js
+++ Udemy/JS/JS-Fundamentals-Part 1/script.js
@@ -87,4 +87,10 @@ if (scoreDolphins > scoreKoalas) {
 } else {
     console.log(`Both win the trophy`);
 }
+
+
+const bill = 400;
+const tip = bill >= 50 && bill <= 300 ? bill * (15 /100) : bill * (20 /100);
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
 */
