fcefa997fada63619c824abbccd5babd651d7eed
dianGrigorov <diangrigorov@gmail.com>
2023-11-09 08:41:26 +0200

    Update script.js

---
+++ Udemy/JS/JS-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,28 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can Drive`);
+
+           //FUNCTIONS
+
+function logger() {
+    console.log(`My name is Dian`);
+}
+
+// calling / running / invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} appels and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const applesOrangeJuice = fruitProcessor(2, 4);
+console.log(applesOrangeJuice); 
+*/
