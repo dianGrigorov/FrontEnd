6c1d3df2c91262f90509135f4f184a70fb7b542e
dianGrigorov <diangrigorov@gmail.com>
2023-11-09 07:38:21 +0200

    Update script.js

--- Udemy/JS/JS-Fundamentals-Part 1/script.js
+++ Udemy/JS/JS-Fundamentals-Part 1/script.js
@@ -90,7 +90,7 @@ if (scoreDolphins > scoreKoalas) {
 
 
 const bill = 400;
-const tip = bill >= 50 && bill <= 300 ? bill * (15 /100) : bill * (20 /100);
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
 */
