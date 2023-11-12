450bcd9e1f19de2c4b4fa703b7b9528bb32c545f
dianGrigorov <diangrigorov@gmail.com>
2023-11-12 18:58:11 +0200

    Update script.js

--- Udemy/JS/JS-Fundamentals-Part-2/script.js
+++ Udemy/JS/JS-Fundamentals-Part-2/script.js
@@ -184,7 +184,33 @@ const dido = {
 console.log(dido.calcAge());
 console.log(dido.fetchInfo());
 
-*/ 
+
             // Challenge 3
        
-            
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    
+    calcBMI: function () {
+         this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}            
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+
+    calcBMI:  function()  {
+      this.bmi = this.mass / (this.height * this.height);
+      return this.bmi;
+    }
+}
+
+console.log(mark.calcBMI() > john.calcBMI() 
+? `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})!` 
+: `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})!`);
+
+*/ 
