d6eab12b7cfe51920ad69e146f7d8dff1c0116ae
dianGrigorov <diangrigorov@gmail.com>
2023-11-14 07:07:36 +0200

    .

--- Udemy/JS/04.DOM-Modasl/script.js
+++ Udemy/JS/04.DOM-Modasl/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const closeMOdal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const openModal = function () {
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden')
+}
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', openModal);
+} 
+// closing the modal with button ant click
+btnCloseModal.addEventListener('click',closeMOdal);
+overlay.addEventListener('click', closeMOdal);
+
+// closing the modal with ESC key
+
+document.addEventListener('keydown', function (e) {
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeMOdal();
+    }
+})
