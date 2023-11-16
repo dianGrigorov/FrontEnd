7bdc4e79bf0b826c33b8b6b7db0ce01bc49d83c6
dianGrigorov <diangrigorov@gmail.com>
2023-11-16 12:26:30 +0200

    .

--- Udemy/JS/05.DOM-Pig-Game/script.js
+++ Udemy/JS/05.DOM-Pig-Game/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Selecting elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+// Starting values
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
+
