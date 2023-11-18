4de8847fffce4b8cc72d30119618d89c50abea3e
dianGrigorov <diangrigorov@gmail.com>
2023-11-18 23:10:22 +0200

    .

--- Udemy/JS/05.DOM-Pig-Game/script.js
+++ Udemy/JS/05.DOM-Pig-Game/script.js
@@ -9,20 +9,36 @@ const current0Element = document.getElementById('current--0');
 const current1Element = document.getElementById('current--1');
 
 const diceElement = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn-new');
+const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 
 // Starting values
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add('hidden');
 
-const scores = [0, 0];
-let currScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currScore, activePlayer, playing;
+
+const init = function () {
+
+    scores = [0, 0];
+    currScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0Element.textContent = 0;
+    score1Element.textContent = 0;
+    current0Element.textContent = 0;
+    current1Element.textContent = 0;
+
+    diceElement.classList.add('hidden');
+    player0Element.classList.remove('player--winner');
+    
+    player1Element.classList.remove('player--winner');
+    player0Element.classList.add('player--active');
+    player1Element.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = () => {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -65,6 +81,7 @@ btnHold.addEventListener('click', function () {
         // Finish the game
         if (scores[activePlayer] >= 10) {
             playing = false;
+            diceElement.classList.add('hidden');
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
         } else {
@@ -74,3 +91,5 @@ btnHold.addEventListener('click', function () {
         }
     }
 });
+
+btnNew.addEventListener('click', init)
