6bfc98e060c66da3dbf19cd5994453d84751234d
dianGrigorov <diangrigorov@gmail.com>
2023-11-27 00:04:51 +0200

    .

--- Udemy/JS/06-Data-Structures-Operators/script.js
+++ Udemy/JS/06-Data-Structures-Operators/script.js
@@ -1,54 +1,128 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+
+//   order: function (menuIndex, categoriesIndex) {
+//     return [this.mainMenu[menuIndex], this.categories[categoriesIndex]]
+//   }
+// };
+
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
+};
+// Task 1
+const [players1 ,players2] = game.players;
 
-  order: function (menuIndex, categoriesIndex) {
-    return [this.mainMenu[menuIndex], this.categories[categoriesIndex]]
-  }
+// Task 2
+const [gk, ...fieldPlayers] = [...players1];
+// Task 3
+const [...allPlayers] = [...players1, ...players2];
+// Task 4
+const [...players1Final] = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// Task 5 Based on the game.odds object, create one variable for each odd (called'team1', 'draw' and 'team2')
+
+// const {team1, x: draw, team2} = ({...game.odds});
+const {odds : {team1, x: draw, team2}} = game;
+console.log(team1, draw, team2);
+// Task 6
+function printGoals (...players) {
+ console.log(`${players.length} goals were scored`);
 };
 
-const rest1 = {
-  name: `Dunavski`,
-  numGest: 20,
-};
+printGoals('Dido');
+printGoals('Dido', 'Sasho');
+printGoals(...game.scored)
 
-const rest2 = {
-  name: `Ribarska`,
-  owner: `Anton`,
-};
+// Task 7
+
+const winner = ((game.odds.team1 < game.odds.team2) && game.team1) || game.team2;
+
+console.log(winner);
+// const rest1 = {
+//   name: `Dunavski`,
+//   numGest: 20,
+// };
+
+// const rest2 = {
+//   name: `Ribarska`,
+//   owner: `Anton`,
+// };
 //OR assignment operator 
 // rest1.numGest = rest1.numGest || 10;
 // rest2.numGest = rest2.numGest || 10;
 
-rest1.numGest ||= 10;
-rest2.numGest ||= 10;
-console.log(rest1);
-console.log(rest2);
+// rest1.numGest ||= 10;
+// rest2.numGest ||= 10;
+
+// ?? Nullish assignment operator (null or undefined) 
+// rest1.numGest ??= 10;
+// rest2.numGest ??= 10;
+// console.log(rest1);
+// console.log(rest2);
 // Destructing arrays
 
 // const [a, b] = restaurant.categories;
