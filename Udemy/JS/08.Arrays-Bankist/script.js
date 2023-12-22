536eeded69e3537f6813bea2c9b53757ac824e21
dianGrigorov <diangrigorov@gmail.com>
2023-12-22 09:10:45 +0200

    Update script.js

--- Udemy/JS/08.Arrays-Bankist/script.js
+++ Udemy/JS/08.Arrays-Bankist/script.js
@@ -153,7 +153,7 @@ btnLogin.addEventListener('click', function (e) {
 
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
-  
+
     // Update UI
     createUI(currentAccount);
   }
@@ -180,6 +180,24 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (inputCloseUsername.value === currentAccount.username
+    && Number(inputClosePin.value) === currentAccount.pin) {
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    //Hide UI
+
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
