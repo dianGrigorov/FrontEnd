2a64b27fb95eafde9747c2dde994c4eb39654a08
dianGrigorov <diangrigorov@gmail.com>
2024-01-11 10:23:26 +0200

    Update script.js

--- Udemy/JS/10.AdvancedDom-Bankist/script.js
+++ Udemy/JS/10.AdvancedDom-Bankist/script.js
@@ -70,7 +70,9 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   // Matching strategy 
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    if (id !== '#'){
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
   }
 });
 
