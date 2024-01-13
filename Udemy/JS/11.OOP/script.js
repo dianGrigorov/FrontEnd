e4eaf033bf065a03f430eec7fb9001fcb44540d4
dianGrigorov <diangrigorov@gmail.com>
2024-01-13 23:21:17 +0200

    Update script.js

--- Udemy/JS/11.OOP/script.js
+++ Udemy/JS/11.OOP/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Constructor function
+
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+};
+
+const dido = new Person('Dian', 1986);
+console.log(dido);
